b28732ec21cecd23c4935ae82b922b18c4688e69
thePlague12 <jtrinidadm1296@gmail.com>
2024-05-17 21:21:17 -0600

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import image1 from "./assents/rick-morty.png";
 import { useState } from "react";
-import Character from "./Character.js";
-
-async function restAPI() {}
+import Character from "./character.js";
 
 function App() {
   const [character, setCharacter] = useState(null);
 
   const api = async () => {
     const response = await fetch("https://rickandmortyapi.com/api/character");
+    console.log(response);
     const characterApi = await response.json();
+    console.log(characterApi.results);
     setCharacter(characterApi.results);
   };
 
@@ -18,13 +18,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="title"> Rick and Morty</h1>
-
-        <Character />
-
-        <img src={image1} className="img-home" alt="logo" />
-        <button className="btn-search" onClick={restAPI}>
-          Buscar Personajes
-        </button>
+        {character ? (
+          <Character characters={character} />
+        ) : (
+          <>
+            <img src={image1} className="img-home" alt="logo" />
+            <button className="btn-search" onClick={api}>
+              Buscar Personajes
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
--- src/character.js
+++ src/character.js
@@ -1,5 +1,43 @@
 import React from "react";
 
-export default function Characters() {
-  return <div>Characters</div>;
+export default function Characters(props) {
+  const characters = props.characters;
+  console.log(props.Characters);
+  return (
+    <div className="characters">
+      <h1>Personajes</h1>
+      <span className="back-home">Home</span>
+      <div className="container-characters">
+        {characters.map((character, index) => (
+          <div className="character-container" key={index}>
+            <p>{character.name}</p>
+
+            <img src={character.image}></img>
+            <div>
+              <h3>{character.name}</h3>
+              <h6>
+                {character.status === "Alive" ? (
+                  <>
+                    <span className="alive">Vivo</span>
+                  </>
+                ) : (
+                  <>
+                    <span className="dead">Muerto</span>
+                  </>
+                )}
+              </h6>
+              <p>
+                <span className="text-grey">Episodios:</span>
+                <span>{character.episode.length}</span>
+              </p>
+              <p>
+                <span className="text-grey">Especie:</span>
+                <span>{character.species}</span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
